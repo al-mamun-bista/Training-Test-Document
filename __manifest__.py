90601b094867b0da78ef6c4081c881177568bf23
Golam Mostafa <sujon1mostafa@gmail.com>
2021-06-07 12:06:54 +0600

    basic structure

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Bista Product Group',
+    'version' : '1.0',
+    'sequence' : 1,
+    'summary': 'Bista Product Group Summary',
+    'description' : 'Bista Product Group Description.',
+    'depends' : ['base'],
+    'data' : [
+        'security/product_group_security.xml',
+        'security/ir.model.access.csv',
+
+        'views/product_group_views.xml',
+    ],
+    'installable' : True,
+    'application' : True,
+    'auto_install' : False,
+}
---
+++ models/product_group.py
@@ -0,0 +1,2 @@
+from odoo import api, models, fields, _
+from odoo.exceptions import UserError
